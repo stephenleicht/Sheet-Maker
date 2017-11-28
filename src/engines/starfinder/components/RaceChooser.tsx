6e1158b658f80359099f8a11cae4371d6250e5f7
Stephen Leicht <stephen.leicht@gmail.com>
2017-11-27 21:31:28 -0500

    Prototype to try out stuff for custom components in the ghoti editor

--- src/engines/starfinder/components/RaceChooser.tsx
+++ src/engines/starfinder/components/RaceChooser.tsx
@@ -1,11 +1,35 @@
 import * as React from 'react';
-import { FormElementProps, FormElement } from 'ghoti';
+import { FormElement, FormElementProps, Select } from 'ghoti/forms';
 
 class RaceChooser extends React.Component<FormElementProps, {}> {
+    static defaultProps = {
+        value: {
+            raceName: 'lashunta'
+        }
+    };
+
+    onFieldChange = (fieldName: string, newValue: any) => {
+        const fullNewValue = {
+            ...this.props.value,
+            [fieldName]: newValue
+        };
+
+        this.props.onChange(fullNewValue);
+    }
+
     render() {
         return (
             <div>
-                Custom Race chooser element
+                Subspecies
+                <Select
+                    onChange={(newValue: any) => this.onFieldChange('subspecies', newValue)}
+                    value={this.props.value.subspecies}
+                    options={[
+                        { key: undefined, displayValue: '' },
+                        { key: 'damaya', displayValue: 'Damaya' },
+                        { key: 'korasha', displayValue: 'Korasha' }
+                    ]}
+                />
             </div>
         )
     }
